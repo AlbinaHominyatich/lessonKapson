8dadc7a5407882f28e10a7f2b052c1b9df4044f1
AlbinaHominyatich <hominyatichalbina@gmail.com>
2022-11-05 12:58:53 +0200

    батьки

---
+++ main.py
@@ -0,0 +1,13 @@
+class Grandparents:
+    height = 170
+    age = 60
+    sick = "COVID"
+class Parents(Grandparents):
+     age = 40
+class Children(Parents):
+     height = 150
+     def __init__(self):
+         print(self.sick)
+         print(self.age)
+         print(self.height)
+daun = Children()
